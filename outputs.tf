c29b8d7377c62b84a0c540c75b0ec75325731b4d
Adan Alvarez <adan.alvarez.90@gmail.com>
2022-11-19 22:23:41 +0100

    first import

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "flarevm_ip" {
+  description = "FlareVM IP"
+  value       = var.enable_guacamole == false ? aws_instance.flarevm.public_ip : aws_instance.flarevm.private_ip
+}
+
+output "guacamole_public_ip" {
+  description = "Guacamole public IP"
+  value       = var.enable_guacamole == false ? null : aws_instance.guacamole[0].public_ip
+}
+
+output "guacamole_credentials" {
+  description = "Guacamole credentials"
+  value       = var.enable_guacamole == false ? null : data.external.guacamole_credentials[0].result
+}
